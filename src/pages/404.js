cb982750c01900a240b7c89679e7ac958a71f6b7
Voltstro <me@voltstro.dev>
2021-12-05 15:15:43 +1000

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,23 @@
 import React from "react"
+import Helmet from "react-helmet";
+
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 export default () => (
     <div>
-        <h1>404 - Oh no's! We couldn't find that page :(</h1>
+        <NavBar/>
+        <main className="flex-shrink-0 py-5 bg-white">
+            <section className="py-5 text-center container">
+                <h1>404 - Not Found</h1>
+                <div className="col-lg-6 mx-auto">
+                    <p className="lead mb-4">Your requested file was not found!</p>
+                </div>
+            </section>
+        </main>
+        <Footer/>
+        <Helmet>
+            <title>404 - Team-Capture</title>
+        </Helmet>
     </div>
 )
